6eea9ac8f3704c906f2f7422ca934aec8f2db5ee
AbbasSavvy <abbasteen@gmail.com>
2021-05-14 14:10:57 +0530

    Landing page Slider and Products linked to DB

--- ecom/home/views.py
+++ ecom/home/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 
 
 # Create your views here.
-from product.models import Category
+from product.models import Category, Product
 
 
 def index(request):
     category = Category.objects.all()
+    sliderProducts = Product.objects.all()
     page = 'home'
-    context = {'page': page, 'category': category}
+    context = {'page': page,
+               'category': category,
+               'sliderProducts': sliderProducts}
     return render(request, 'index.html', context)
