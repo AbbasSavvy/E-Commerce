05fba67f2ac461d4b51f44dc0f6fd84932912e26
proacc2022 <proacc2022@gmail.com>
2021-05-16 02:16:18 +0530

    Add to Cart Done

---
+++ ecom/cart/views.py
@@ -0,0 +1,77 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from django.utils.crypto import get_random_string
+
+from cart.models import ShopCart, ShopCartForm
+from product.models import Category, Product
+from user.models import User1Profile, User2Profile
+
+# Create your views here.
+def index(request):
+    return HttpResponse("order Page")
+
+@login_required(login_url='/login') # Check login
+def addtoshopcart(request,id):
+    url = request.META.get('HTTP_REFERER')  # get last url
+    current_user = request.user  # Access User Session information
+    product= Product.objects.get(pk=id)
+
+    checkinproduct = ShopCart.objects.filter(product_id=id, user_id=current_user.id) # Check product in shopcart
+    if checkinproduct:
+        control = 1
+    else:
+        control = 0
+
+    if request.method == 'POST':  # if there is a post
+        form = ShopCartForm(request.POST)
+        if form.is_valid():
+            if control==1: # Update  shopcart
+                data = ShopCart.objects.get(product_id=id, user_id=current_user.id)
+                data.quantity += form.cleaned_data['quantity']
+                data.save()  # save data
+            else: # Insert to Shopcart
+                data = ShopCart()
+                data.user_id = current_user.id
+                data.product_id = id
+                data.quantity = form.cleaned_data['quantity']
+                data.save()
+        messages.success(request, "Product added to Shopcart ")
+        return HttpResponseRedirect(url)
+
+    else: # if there is no post
+        if control == 1:  # Update  shopcart
+            data = ShopCart.objects.get(product_id=id, user_id=current_user.id)
+            data.quantity += 1
+            data.save()  #
+        else:  #  Inser to Shopcart
+            data = ShopCart()  # model ile bağlantı kur
+            data.user_id = current_user.id
+            data.product_id = id
+            data.quantity = 1
+            data.save()  #
+        messages.success(request, "Product added to Shopcart")
+        return HttpResponseRedirect(url)
+
+def shopcart(request):
+    category = Category.objects.all()
+    current_user = request.user  # Access User Session information
+    shopcart = ShopCart.objects.filter(user_id=current_user.id)
+    total=0
+    for rs in shopcart:
+        total += rs.product.product_discount * rs.quantity
+    #return HttpResponse(str(total))
+    context={'shopcart': shopcart,
+             'category':category,
+             'total': total,
+             }
+    return render(request,'shopcart_products.html',context)
+
+@login_required(login_url='/login') # Check login
+def deletefromcart(request,id):
+    ShopCart.objects.filter(id=id).delete()
+    messages.success(request, "Your item deleted form Shopcart.")
+    return HttpResponseRedirect("/shopcart")
