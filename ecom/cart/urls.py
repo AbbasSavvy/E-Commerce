eafd7707f1a43eb38ffc1a4080bd26035a6fe253
proacc2022 <proacc2022@gmail.com>
2021-05-16 10:50:42 +0530

    Quantity Change and verification done

--- ecom/cart/urls.py
+++ ecom/cart/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('addtoshopcart/<int:id>', views.addtoshopcart, name='addtoshopcart'),
     path('deletefromcart/<int:id>', views.deletefromcart, name='deletefromcart'),
+    path('addproduct/<int:id>', views.addproduct, name='addproduct'),
+    path('reduceproduct/<int:id1>/<int:id2>', views.reduceproduct, name='reduceproduct'),
 ]
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -31,29 +31,50 @@ def addtoshopcart(request,id):
         if form.is_valid():
             if control==1: # Update  shopcart
                 data = ShopCart.objects.get(product_id=id, user_id=current_user.id)
-                data.quantity += form.cleaned_data['quantity']
-                data.save()  # save data
+                quantity_desired= data.quantity +form.cleaned_data['quantity']
+                leftover= product.product_stock-quantity_desired
+                if leftover>=0:
+                    data.quantity += form.cleaned_data['quantity']
+                    data.save()  # save data
+                    messages.success(request, "Product added to Shopcart ")
+                else:
+                    messages.info(request, "Out of Stock. Decrease your quantity and try.")
             else: # Insert to Shopcart
                 data = ShopCart()
                 data.user_id = current_user.id
                 data.product_id = id
-                data.quantity = form.cleaned_data['quantity']
-                data.save()
-        messages.success(request, "Product added to Shopcart ")
+                leftover = product.product_stock - form.cleaned_data['quantity']
+                if leftover>=0:
+                    data.quantity = form.cleaned_data['quantity']
+                    data.save()
+                    messages.success(request, "Product added to Shopcart ")
+                else:
+                    messages.info(request, "Out of Stock. Decrease your quantity and try.")
+
         return HttpResponseRedirect(url)
 
     else: # if there is no post
         if control == 1:  # Update  shopcart
             data = ShopCart.objects.get(product_id=id, user_id=current_user.id)
-            data.quantity += 1
-            data.save()  #
+            quantity_desired = data.quantity + 1
+            leftover = product.product_stock - quantity_desired
+            if leftover >= 0:
+                data.quantity += 1
+                data.save()
+                messages.success(request, "1 item added to Shopcart ")
+            else:
+                messages.info(request, "Out of stock. Cannot add item")
         else:  #  Inser to Shopcart
             data = ShopCart()  # model ile bağlantı kur
             data.user_id = current_user.id
             data.product_id = id
-            data.quantity = 1
-            data.save()  #
-        messages.success(request, "Product added to Shopcart")
+            leftover = product.product_stock - 1
+            if leftover >= 0:
+                data.quantity = 1
+                data.save()  #
+                messages.success(request, "Product added to Shopcart ")
+            else:
+                messages.info(request, "Out of Stock.")
         return HttpResponseRedirect(url)
 
 def shopcart(request):
@@ -74,4 +95,29 @@ def shopcart(request):
 def deletefromcart(request,id):
     ShopCart.objects.filter(id=id).delete()
     messages.success(request, "Your item deleted form Shopcart.")
+    return HttpResponseRedirect("/shopcart")
+
+@login_required(login_url='/login') # Check login
+def addproduct(request,id):
+    current_user = request.user  # Access User Session information
+    product= Product.objects.get(pk=id)
+    data = ShopCart.objects.get(product_id=id, user_id=current_user.id)
+    data.quantity += 1
+    data.save()  #
+
+    messages.success(request, "Product added to Shopcart")
+    return HttpResponseRedirect("/shopcart")
+
+@login_required(login_url='/login') # Check login
+def reduceproduct(request,id1,id2):
+    current_user = request.user  # Access User Session information
+    product= Product.objects.get(pk=id1)
+    data = ShopCart.objects.get(product_id=id1, user_id=current_user.id)
+    data.quantity -= 1
+    data.save()
+    if data.quantity==0:
+        ShopCart.objects.filter(id=id2).delete()
+        messages.success(request, "Your item deleted form Shopcart.")
+    else:
+        messages.success(request, "Product reduced from Shopcart")
     return HttpResponseRedirect("/shopcart")
