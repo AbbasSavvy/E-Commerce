09557c5a0e6cf3a6399c1299e867ff23128a8aea
proacc2022 <proacc2022@gmail.com>
2021-05-19 20:44:02 +0530

    Many small things done

--- ecom/user/forms.py
+++ ecom/user/forms.py
@@ -9,29 +9,54 @@ from django.forms import inlineformset_factory
 
 
 class SignUp1Form(UserCreationForm):
-    username = forms.CharField(max_length=30,label= 'User Name :')
-    email = forms.EmailField(max_length=200,label= 'Email :')
-    first_name = forms.CharField(max_length=100, help_text='First Name',label= 'First Name :')
-    last_name = forms.CharField(max_length=100, help_text='Last Name',label= 'Last Name :')
-
+    username = forms.CharField(max_length=30,label= 'User Name :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px','size':'10'}))
+    email = forms.EmailField(max_length=200,label= 'Email :',
+                           widget= forms.EmailInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
+    first_name = forms.CharField(max_length=100, help_text='First Name',label= 'First Name :',
+                                 widget=forms.TextInput
+                                 (attrs={'class': 'form-control','style':'width:500px'}))
+    last_name = forms.CharField(max_length=100, help_text='Last Name',label= 'Last Name :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
+    password1 = forms.CharField(max_length=100, help_text='Password',label= 'Password :',
+                           widget= forms.PasswordInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
+    password2 = forms.CharField(max_length=100, help_text='Password', label='Password Confirmation :',
+                                widget=forms.PasswordInput
+                                (attrs={'class': 'form-control', 'style': 'width:500px'}))
 
     class Meta:
         model = User
         fields = ('username', 'email','first_name','last_name', 'password1', 'password2', )
 
 class SignUp2Form(forms.ModelForm):
-    address = forms.CharField(max_length=100,label= 'Address :')
-    city = forms.CharField(max_length=100,label= 'City :')
-    state = forms.CharField(max_length=100,label= 'State :')
-    pin_code = forms.CharField(max_length=100,label= 'Pin Code :')
-    country = forms.CharField(max_length=100,label= 'Country :')
+    address = forms.CharField(max_length=100,label= 'Address :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
+    city = forms.CharField(max_length=100,label= 'City :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
+    state = forms.CharField(max_length=100,label= 'State :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
+    pin_code = forms.IntegerField(label= 'Pin Code :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
+    country = forms.CharField(max_length=100,label= 'Country :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
 
     class Meta:
         model = User1Profile
         fields = ('address', 'city','state','pin_code','country')
 
 class SignUp3Form(forms.ModelForm):
-    phone = forms.CharField(max_length=100,label= 'Phone :')
+    phone = forms.IntegerField(label= 'Phone :',
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control','style':'width:500px'}))
 
     class Meta:
         model = User2Profile
