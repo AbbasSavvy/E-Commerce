b8514e9b31607bbf696dd8b8eb31e23d9c306772
AbbasSavvy <abbasteen@gmail.com>
2021-05-31 12:24:27 +0530

    random

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -123,11 +123,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # ...
 SITE_ID = 1
